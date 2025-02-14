fca09e26fd59721754d612a91d36873f11099ac4
aliagayevv <aghayev_233@mail.ru>
2025-02-14 15:14:16 +0400

    problem 125 solved

---
+++ 125. Valid Palindrome/solution.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s) {
+  s = s.toLowerCase().replace(/[^a-z0-9]/g, "");
+
+  var left = 0;
+  var right = s.length - 1;
+
+  while (left < right) {
+    if (s[left] !== s[right]) return false;
+    left++;
+    right--;
+  }
+
+  return true;
+};
