c383d91d41e7df8cf26c891e3c2103dec3cee3a2
aliagayevv <aghayev_233@mail.ru>
2025-02-14 04:28:44 +0400

    704 processing...

---
+++ 704. Binary Search/solution.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+  if (nums.length === 1 && target === nums[0]) return 0;
+  if (nums.length === 1 && target !== nums[0]) return -1;
+  nums.sort((a, b) => a - b);
+
+  var left = 0;
+  var right = nums.length - 1;
+
+  while (right > left) {
+    var mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return -1;
+};
+
+console.log(search([2, 5], 5));
